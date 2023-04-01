a031b63ca5db254559d8058abbe19d07f66383c2
Shuwen <shuwenf2000@gmail.com>
2023-04-01 14:17:33 -0700

    fixed js

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 
 function displayAccount() {
+    // add to accounts page
     let accountName = document.querySelector("#accountName");
     let pretax_accounts = document.querySelector("#pretax-accounts");
     let newRow = pretax_accounts.insertRow();
     let newCell = newRow.insertCell();
     newCell.textContent = accountName.value();
     
-    // add to inputs page
-    const accountsTable = document.querySelector("#accounts-table");
-    accounts.forEach((account) => {
-    const newRow = accountsTable.insertRow();
-    const accountNameCell = newRow.insertCell(0);
-    accountNameCell.innerHTML = account;
-    });
+    accountName.value = "";
 
     // save to storage
     let accounts = JSON.parse(localStorage.getItem("accounts")) || [];
-    accounts.push(accountName.value);
+    accounts.push(newCell.textContent);
     localStorage.setItem("accounts", JSON.stringify(accounts));
 
-    accountName.value = "";
+    // add to inputs page
+    const accountsTable = document.querySelector("#accounts-table");
+    accounts.forEach((account) => {
+        const newRow = accountsTable.insertRow();
+        const accountNameCell = newRow.insertCell(0);
+        accountNameCell.innerHTML = account;
+    });
 }
