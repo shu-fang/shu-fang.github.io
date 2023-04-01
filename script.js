12c9b2e29fe7c19a5a936f0d124d2fdd2d6598e0
Shuwen <shuwenf2000@gmail.com>
2023-04-01 14:24:36 -0700

    bug fix

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
 function displayAccount() {
-    // add to accounts page
+    // save to storage
+    let accounts = JSON.parse(localStorage.getItem("accounts")) || [];
     let accountName = document.querySelector("#accountName");
+    accounts.push(accountName.value);
+    localStorage.setItem("accounts", JSON.stringify(accounts));
+    
+    // add to accounts page
     let pretax_accounts = document.querySelector("#pretax-accounts");
     let newRow = pretax_accounts.insertRow();
     let newCell = newRow.insertCell();
     newCell.textContent = accountName.value();
     
     accountName.value = "";
-
-    // save to storage
-    let accounts = JSON.parse(localStorage.getItem("accounts")) || [];
-    accounts.push(newCell.textContent);
-    localStorage.setItem("accounts", JSON.stringify(accounts));
 
     // add to inputs page
     const accountsTable = document.querySelector("#accounts-table");
