12b42c06d845c5f56ad1f740002e1512937504d4
Shuwen <shuwenf2000@gmail.com>
2023-04-01 13:57:35 -0700

    put script into separate js file

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ accounts.forEach((account) => {
   const accountNameCell = newRow.insertCell(0);
   accountNameCell.innerHTML = account;
 });
+
+function displayAccount() {
+    let accountName = document.querySelector("#accountName");
+    let pretax_account = document.querySelector("#pretax-accounts");
+    let newRow = pretax_accounts.insertRow();
+    let newCell = newRow.insertCell();
+    newCell.textContent = accountName.value();
+    accountName.value = "";
+
+    // save to storage
+    let accounts = JSON.parse(localStorage.getItem("accounts")) || [];
+    accounts.push(accountName.value);
+    localStorage.setItem("accounts", JSON.stringify(accounts));
+}
