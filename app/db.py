ee87f0c308385260b0c2a91bdf54a33b57ef6109
Shuwen <shuwenf2000@gmail.com>
2023-04-02 02:06:05 -0700

    debug

--- app/db.py
+++ app/db.py
@@ -1,13 +1,17 @@
 import sqlite3
 
-conn = sqlite3.connect("accounts.sqlite")
+try:
+    conn = sqlite3.connect("accounts.sqlite")
+    cursor = conn.cursor()
 
-cursor = conn.cursor()
+    sql_query = """CREATE TABLE IF NOT EXISTS accounts (
+        name text NOT NULL,
+        type text NOT NULL DEFAULT "unknown",
+        tax_status text NOT NULL DEFAULT "unknown",
+        balances text NOT NULL DEFAULT "0"
+    )"""
 
-sql_query = """ CREATE TABLE IF NOT EXISTS accounts (
-    name text NOT NULL,
-    type text NOT NULL DEFAULT "unknown",
-    tax_status text NOT NULL DEFAULT "unknown",
-    balances text NOT NULL DEFAULT "0"
-)"""
-cursor.execute(sql_query)
+    cursor.execute(sql_query)
+
+except sqlite3.Error as e:
+    print(f"Error creating table: {e}")
