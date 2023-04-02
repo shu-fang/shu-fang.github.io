d7e539d15716700cc47d22f0f24dc61bfce30b1e
Shuwen <shuwenf2000@gmail.com>
2023-04-02 12:16:16 -0700

    add new account column to entries table whenever new accounts are added

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, flash, g, send_from_directory
 import sqlite3
-from . import db
-# import db
+# from . import db
+import db
 from flask import jsonify
 # from jinja2 import Environment, FileSystemLoader
 
@@ -45,15 +45,9 @@ def accounts():
 
 @app.route('/input', methods=['GET','POST'])
 def input():
-    conn = sqlite3.connect('entries.sqlite')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM entries')
-    entries = cursor.fetchall()
-    conn.close()
-
     conn = db_connection()
     cursor = conn.cursor()
-
+    # update account balance
     if request.method == 'POST':
         for account in request.form:
             if account.startswith('balance--'):
@@ -66,10 +60,18 @@ def input():
 
     cursor.execute("SELECT name, balances FROM accounts")
     pretax_accounts = get_pretax_accounts()
-    return render_template('input.html', pretax_accounts=pretax_accounts, entries=entries)
+    conn.close()
+
+    entries_conn = sqlite3.connect('entries.sqlite')
+    cursor = entries_conn.cursor()
+    cursor.execute('SELECT * FROM entries')
+    entries = cursor.fetchall()
+    entries_conn.close()
+    return render_template('input.html', pretax_accounts=pretax_accounts, entries=entries, cursor=cursor)
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    # insert new account into accounts database
     conn = db_connection()
     cursor = conn.cursor()
     new_name = request.form['accountName']
@@ -78,6 +80,17 @@ def submit():
     sql = """INSERT INTO accounts (name, type, tax_status)
                 VALUES (?, ?, ?)"""
     cursor = cursor.execute(sql, (new_name, new_type, new_tax_status))
+    conn.commit()
+    conn.close()
+
+    # insert new account as column into entries database
+    conn = sqlite3.connect('entries.sqlite')
+    cursor = conn.cursor()
+    # Construct a new SQL query to add a new column to the 'entries' table
+    sql_query = f"ALTER TABLE entries ADD COLUMN {new_name} REAL DEFAULT 0"
+    
+    # Execute the SQL query and commit the changes to the database
+    cursor.execute(sql_query)
     conn.commit()
     conn.close()
     return jsonify({'name': new_name, 'type': new_type, 'tax_status': new_tax_status}), 200
