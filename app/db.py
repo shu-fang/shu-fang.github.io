038dd251b619e5f8a22b0539376f86dfa5afd127
Shuwen <shuwenf2000@gmail.com>
2023-04-02 11:54:08 -0700

    added new entries database

--- app/db.py
+++ app/db.py
@@ -12,6 +12,25 @@ try:
     )"""
 
     cursor.execute(sql_query)
+    conn.commit()
+    conn.close()
+
+except sqlite3.Error as e:
+    print(f"Error creating table: {e}")
+
+
+try:
+    conn = sqlite3.connect("entries.sqlite")
+    cursor = conn.cursor()
+
+    sql_query = """CREATE TABLE IF NOT EXISTS entries (
+        date DATE NOT NULL DEFAULT (DATE('now', 'localtime')),
+        balances TEXT NOT NULL DEFAULT "0"
+    )"""
+
+    cursor.execute(sql_query)
+    conn.commit()
+    conn.close()
 
 except sqlite3.Error as e:
     print(f"Error creating table: {e}")
